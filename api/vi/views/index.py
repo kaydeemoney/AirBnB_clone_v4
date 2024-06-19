5e11f5fa03f1604a0b68f56b0dbf0ae0b39bff70
kaydeemoney <daramola4082@gmail.com>
2024-06-19 01:45:20 -0700

    without images

--- api/vi/views/index.py
+++
@@ -1,7 +0,0 @@
-from api.v1.views import app_views
-from flask import jsonify
-
-# Define a route /status on the object app_views that returns a JSON: {"status": "OK"}
-@app_views.route('/status', methods=['GET'])
-def status():
-    return jsonify({"status": "OK"})
--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -51,6 +51,17 @@ class DBStorage:
                     new_dict[key] = obj
         return (new_dict)
 
+    def get(self, cls, id):
+        """retrieves an object of a class with id"""
+        obj = None
+        if cls is not None and issubclass(cls, BaseModel):
+            obj = self.__session.query(cls).filter(cls.id == id).first()
+        return obj
+
+    def count(self, cls=None):
+        """retrieves the number of objects of a class or all (if cls==None)"""
+        return len(self.all(cls))
+
     def new(self, obj):
         """add the object to the current database session"""
         self.__session.add(obj)
@@ -74,19 +85,3 @@ class DBStorage:
     def close(self):
         """call remove() method on the private session attribute"""
         self.__session.remove()
-
-    def get(self, cls, id):
-        """
-        Retrieves one object based on class name and id.
-        """
-        if cls and id:
-            key = f"{cls.__name__}.{id}"
-            return self.all(cls).get(key)
-        return None
-
-    def count(self, cls=None):
-        """
-        Counts the number of objects in storage matching the given class name.
-        If no class name is provided, returns the count of all objects in storage.
-        """
-        return len(self.all(cls))
